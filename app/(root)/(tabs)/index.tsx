583c98fc4e2c2d87fa2fd28f6a56ef5d7cf5cdd7
wesley <wesleywaka77@gmail.com>
2025-01-10 17:36:52 +0300

    folder set-up

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const index = () => {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Text className='font-bold text-lg my-10'>Welcome to ReState</Text>
+      <Link href='/sign-in'> Sign In</Link>
+      <Link href='/explore'> Explore</Link>
+      <Link href='/profile'> Profile</Link>
+      <Link href='/properties/1'>Properties</Link>
+    </View>
+  )
+}
+
+export default index
